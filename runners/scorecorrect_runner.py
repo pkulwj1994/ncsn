b8580dc271723b5905b99a5fc23781dc38b6f289
William Luo <32350533+pkulwj1994@users.noreply.github.com>
2022-04-13 20:32:55 +0800

    Update scorecorrect_runner.py

--- runners/scorecorrect_runner.py
+++ runners/scorecorrect_runner.py
@@ -405,5 +405,9 @@ class CorrectorRunner():
         shutil.rmtree(self.args.image_folder)
         if iters:
             imgs[0].save("{}_movie.gif".format(iters), save_all=True, append_images=imgs[1:], duration=1, loop=0)
+            try:
+                imgs[0].save("/content/drive/MyDrive/{}_movie.gif".format(iters), save_all=True, append_images=imgs[1:], duration=1, loop=0)
+            except:
+                pass
         else:
             imgs[0].save("movie.gif", save_all=True, append_images=imgs[1:], duration=1, loop=0)
