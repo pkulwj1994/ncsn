cbbe4a7b7d58832e98dde9707dca6b9fc20d1a09
William Luo <32350533+pkulwj1994@users.noreply.github.com>
2022-05-03 20:48:23 +0800

    Update __init__.py

--- runners/__init__.py
+++ runners/__init__.py
@@ -2,5 +2,5 @@ from runners.toy_runner import *
 from runners.scorenet_runner import *
 from runners.anneal_runner import *
 from runners.baseline_runner import *
-from runners.scorecorrect_runner import *
-from runners.scoresteincorrect_runner import *
+from runners.scorecorrect_runner1 import *
+# from runners.scoresteincorrect_runner import *
