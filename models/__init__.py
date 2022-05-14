202c7ee731f118221902785134179cc5c843d99a
William Luo <32350533+pkulwj1994@users.noreply.github.com>
2022-05-14 11:47:41 +0800

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,3 @@
+from models.flowplusplus import FlowPlusPlus, FlowPlusPlus_celeba
+from models.ebm import EBM, EBM_res, EBM_deep32, EBM_deep32_swish, EBM_deep32_3, EBM_res_wA
 __all__ = ['nice', 'gmm', 'cnn', 'autoencoder', 'flows', 'realnvp', 'rbm']
